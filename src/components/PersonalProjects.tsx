6a90376533e5ce25c521f339c19f1f7b997c361a
kareemtahaismail <kareemtahaismail@gmail.com>
2024-05-08 18:34:34 +0300

    added project; game

--- src/components/PersonalProjects.tsx
+++ src/components/PersonalProjects.tsx
@@ -1,12 +1,44 @@
+import { useState } from "react";
+import PlayerPaused from "../assets/images/Player_Paused.gif";
+import PlayerMoving from "../assets/images/Player.gif";
+import { InfoTooltip } from "./CustomComponents";
 import "./styles/PersonalProjects.css";
 const PersonalProjects = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <>
-      <div className="projects">
-        <h1>Personal Projects</h1>
-        <h1>Personal Projects</h1>
-        <h1>Personal Projects</h1>
-        <h1>Personal Projects</h1>
+      <div className="projects unselectable">
+        <div
+          className="project"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <img
+            src={isHovered ? PlayerMoving : PlayerPaused}
+            alt="Fighter.gif"
+          ></img>
+          <h2>2D Platformer Fighting Game</h2>
+          <p className="description">
+            "In a universe brimming with sci-fi tales, one might assume humanity
+            would stand by your side when otherwordly demons reign... Think
+            again!"
+          </p>
+          <p className="techUsed">
+            <b>Technologies used:</b> Godot, Aseprite
+          </p>
+          <InfoTooltip arrow title="Confidential for the time being!">
+            <a href="#"><button>Source code</button></a>
+          </InfoTooltip>
+        </div>
       </div>
     </>
   );
