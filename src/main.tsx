d14cdabf06513e18154198f33e2b4577a6e3f4e9
kareemtahaismail <kareemtahaismail@gmail.com>
2024-05-13 03:39:59 +0300

    attempt 2 failed, revert

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-const base = document.createElement('base')
-base.href = import.meta.env.BASE_URL
-document.head.insertBefore(base, document.head.firstChild)
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
