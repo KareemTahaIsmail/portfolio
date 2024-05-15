5374c858a19b6e57b14e69f6540fde0aea97ea56
kareemtahaismail <kareemtahaismail@gmail.com>
2024-05-15 03:30:08 +0300

    speech bubble, unfinished changes

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./styles/AboutMe.css";
 import Fade from "@mui/material/Fade";
 import userInputUtils from "./utils/userInputUtils";
@@ -8,6 +9,26 @@ const AboutMe = () => {
     "/experience"
   );
   userInputUtils.useTouchNavigation("/projects", "/experience");
+
+  const SpeechBubbleImgSrc = "https://static.vecteezy.com/system/resources/previews/027/517/770/original/pixel-art-speech-bubble-png.png"
+
+  const [demonTooltipMap, setDemonTooltipMap] = useState<Map<string, boolean>>(
+    new Map([
+      ['demon1', false],
+      ['demon2', false],
+      ['demon3', false]
+    ]));
+
+
+  const handleImageClick = (demonKey: string) => {
+    // Create a new map object with the updated value for the specified demon key
+    const updatedMap = new Map(demonTooltipMap);
+    demonTooltipMap.forEach((value, key) => {
+      updatedMap.set(key, key === demonKey);
+    })
+
+    setDemonTooltipMap(updatedMap);
+  };
 
   return (
     <>
@@ -20,15 +41,22 @@ const AboutMe = () => {
             </h1>
           </div>
           <div className="pixelArtDemo">
-            <p>
-              Outside of coding, my passion lies in {" "}
-              <i style={{color: "#646cff"}}>art.</i><br /> That being said.. {" "}
-              <span className="demons">demons</span>
-            </p>
+
+            <div className="speechBubble">
+              <img src={SpeechBubbleImgSrc} className="speechBubbleImage" alt="" />
+              <p>
+                Outside of coding, my passion lies in {" "}
+                <i style={{ color: "#646cff" }}>art.</i><br /> That being said.. {" "}
+                <span className="demons">demons</span>
+              </p>
+            </div>
+
+
             <div className="gifs">
               <DemonTooltip
                 arrow
                 className="unselectable"
+                open={demonTooltipMap.get('demon1')}
                 TransitionComponent={Fade}
                 TransitionProps={{ timeout: 300 }}
                 title="Yerc; the skinless demon, stood as an infernal enigma. Potentially a valuable ally if well-fed, but its insatiable gluttony makes it difficult to befriend."
@@ -37,10 +65,12 @@ const AboutMe = () => {
                   src="https://i.imgur.com/wLzDZrk.gif"
                   alt="Yerc"
                   draggable="false"
+                  onClick={() => handleImageClick('demon1')}
                 />
               </DemonTooltip>
               <DemonTooltip
                 arrow
+                open={demonTooltipMap.get('demon2')}
                 className="unselectable"
                 title="Shuf; the all-seeing demon, lives up to its name with an unhindered gaze. Impossible to catch off guard."
               >
@@ -48,10 +78,12 @@ const AboutMe = () => {
                   src="https://i.imgur.com/nDUg9cr.gif"
                   alt="Shuf"
                   draggable="false"
+                  onClick={() => handleImageClick('demon2')}
                 />
               </DemonTooltip>
               <DemonTooltip
                 arrow
+                open={demonTooltipMap.get('demon3')}
                 className="unselectable"
                 title="Za; the two-headed music demon, hears every note, making music its true love. It possesses ears tuned to every octave, rendering no sound too faint or too piercing for its keen perception, though its taste remains a mystery."
               >
@@ -59,6 +91,7 @@ const AboutMe = () => {
                   src="https://i.imgur.com/O2QfJ2B.gif"
                   alt="Za"
                   draggable="false"
+                  onClick={() => handleImageClick('demon3')}
                 />
               </DemonTooltip>
             </div>
